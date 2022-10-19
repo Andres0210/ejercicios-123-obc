5b680665eb320a4e0b7950057837c76a501ffb74
Andrés0210 <ing_amilkar@hotmail.com>
2022-10-19 18:28:44 -0500

    entrega de tarea

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import ComponenteA from './componentes/componenteA';
 
 function App() {
+  const contactoPrueba = {
+    nombre: 'Amilcar Andres',
+    apellido: 'Ibarra Cuadrado',
+    email: 'ing_amilkar@hotmail.com',
+    conectado: false,
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ComponenteA contacto={contactoPrueba} />
       </header>
     </div>
   );
